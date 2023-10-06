361401a0d5456dfdc9908ebf18f56e67ecb8d43b
Om Bhandari <orbhandari@connect.ust.hk>
2023-10-06 13:23:54 +0800

    Tailwind and leaderboard page

--- frontend/pages/ranking/index.js
+++ frontend/pages/ranking/index.js
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 import PageTemplate from "@components/reusable/template/pageTemplate";
+import Leaderboard from "./ranking-components/Leaderboard"
+
+const data = [
+  { name: 'Apple', score: 100, currency: 12442, ethicspoints: 124 },
+  { name: 'Pear', score: 100, currency: 42, ethicspoints: 124 },
+  { name: 'John', score: 214, currency: 24, ethicspoints: 1244 },
+  { name: 'Jack', score: 21, currency: 424, ethicspoints: 12114 },
+  { name: 'Super', score: 1, currency: 142, ethicspoints: 11424 },
+  
+  // More users...
+];
+
 
 
 function ranking() {
@@ -9,7 +21,7 @@ function ranking() {
 return (
     <>
       <PageTemplate>
-      This is ranking page
+        <Leaderboard data={data}/>
       </PageTemplate>
     </>
   );
---
+++ frontend/pages/ranking/ranking-components/Leaderboard.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+const Leaderboard = ({ data }) => {
+  // Sort the data in descending order based on score
+  const sortedData = [...data].sort((a, b) => b.score - a.score);
+
+  return (
+    <div className="flex flex-col items-center ">
+      <div className="flex-row items-center align-center justify-center mt-2">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-black md:text-5xl lg:text-6x">
+          Who's best at Finance?
+        </h1>
+      </div>
+        
+      <div className="overflow-x-auto">
+        <div className="align-middle inline-block min-w-full">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Photo
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Score
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Currency
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Ethics points
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {sortedData.map((item, index) => (
+                  <tr key={index}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {item.photo}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {item.name}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {item.score}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {item.currency}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {item.ethicspoints}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Leaderboard;
